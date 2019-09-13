bf77484ff809d5f9af1871d2efc04e9193ed184d
Maximilian Helmersson <max.helmersson@gmail.com>
2019-09-13 11:30:22 +0200

    Add #2

--- public/scripts/customer.js
+++ public/scripts/customer.js
@@ -7,3 +7,15 @@ function pageViewer(page) {
   var openPage = document.getElementById(page);
   openPage.style.display = 'block';
 }
+
+var status = 3
+
+function statusCheck() {
+  var checks = document.getElementsByClassName('fa-check');
+
+  for (let i = 0; i <= status; i++) {
+    checks[i].style.display = 'block'
+  }
+}
+
+statusCheck()
