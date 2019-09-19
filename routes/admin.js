6135c979d4f5906124aa34ee0d980ed15b93c807
Maximilian Helmersson <max.helmersson@gmail.com>
2019-09-19 19:56:25 +0200

    Edit #6

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,17 @@
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+const {User} = require('../models/user');
+const {Plan} = require('../models/plan');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', [auth, admin], (req, res) => {
-  res.sendFile('admin.html', { root: './public' });
+router.get('/', [auth, admin], async (req, res) => {
+  const user = await User.findById('5d7f8954b64ccc3948244920');
+  const plan = await Plan.findById(user.plan);
+
+  console.log(plan.dateCreated)
+
+  res.render('admin', { firstName: user.firstName, lastName: user.lastName, country: user.country, budget: plan.budget, date: plan.dateCreated });
 });
 
 module.exports = router;
--- routes/customer.js
+++ routes/customer.js
@@ -8,11 +8,8 @@ const router = express.Router();
 router.get('/', auth, async (req, res) => {
   const user = await User.findById('5d7f8954b64ccc3948244920');
   const plan = await Plan.findById(user.plan);
-  console.log(plan);
 
-  res.render('customer', { usecases: plan.gaming });
-
-  console.log('Done');
+  res.render('customer', { usecases: plan.gaming, sliderOne: plan.sliderOne, sliderTwo: plan.sliderTwo, case1: plan.case, mainColor: plan.mainColor, secondColor: plan.secondColor, budget: plan.budget, name: plan.name, resolution: plan.resolution, fps: plan.fps, setting: plan.setting, comment: plan.comment });
 })
 
 module.exports = router;
