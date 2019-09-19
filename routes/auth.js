ba3f42359e3cefb3b1f605df106ab44d4ab3aa8a
Maximilian Helmersson <max.helmersson@gmail.com>
2019-09-19 20:00:54 +0200

    Edit #6

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile('login.html', { root: './public' });
+  res.render('login');
 });
 
 router.post('/', async (req, res) => {
