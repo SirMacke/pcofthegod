df905d7ac9f6144c1fad7fbf32cbb91dbe0dd53e
Maximilian Helmersson <max.helmersson@gmail.com>
2019-09-20 19:39:42 +0200

    Edit #8

--- routes/admin.js
+++ routes/admin.js
@@ -6,10 +6,37 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', [auth, admin], async (req, res) => {
-  const user = await User.findById('5d7f8954b64ccc3948244920');
+  User.find({}, function(err, users) {
+    var userMap = users;
+    userMap.shift();
+
+    Plan.find({}, function(err, plans) {
+      var planMap = plans;
+
+      console.log(userMap[0 + 1])
+      console.log(planMap[0])
+
+
+      // const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      // var date = months[plan.dateCreated.getMonth()] + ' ' + plan.dateCreated.getDate() + ' ' + plan.dateCreated.getFullYear();
+
+      res.render('admin', { users: userMap, plans: planMap});
+    });
+  });
+
+
+
+
+});
+
+router.get('/:id', [auth, admin], async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(404).send('Invalid user.');
+
   const plan = await Plan.findById(user.plan);
-  
-  res.render('admin', { firstName: user.firstName, lastName: user.lastName, country: user.country, budget: plan.budget, date: plan.dateCreated });
+  if (!user) return res.status(404).send('Invalid plan.');
+
+  res.render('admin_plan', { usecases: plan.gaming, sliderOne: plan.sliderOne, sliderTwo: plan.sliderTwo, case1: plan.case, mainColor: plan.mainColor, secondColor: plan.secondColor, budget: plan.budget, name: plan.name, resolution: plan.resolution, fps: plan.fps, setting: plan.setting, comment: plan.comment });
 });
 
 module.exports = router;
--- routes/customer.js
+++ routes/customer.js
@@ -1,13 +1,22 @@
 const auth = require('../middleware/auth');
 const {User} = require('../models/user');
 const {Plan} = require('../models/plan');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', auth, async (req, res) => {
-  const user = await User.findById('5d7f8954b64ccc3948244920');
+  var token = req.signedCookies.tokenId;
+  const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+
+  const user = await User.findById(decoded._id);
+  if (!user) return res.status(404).send('Invalid user.');
+
   const plan = await Plan.findById(user.plan);
+  if (!plan) return res.status(404).send('Invalid plan.');
+
 
   res.render('customer', { usecases: plan.gaming, sliderOne: plan.sliderOne, sliderTwo: plan.sliderTwo, case1: plan.case, mainColor: plan.mainColor, secondColor: plan.secondColor, budget: plan.budget, name: plan.name, resolution: plan.resolution, fps: plan.fps, setting: plan.setting, comment: plan.comment });
 })
