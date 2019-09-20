8f17f7a7a61383b94e74a64f40bf605016155bc0
Maximilian Helmersson <max.helmersson@gmail.com>
2019-09-20 19:40:40 +0200

    Edit #9

--- routes/admin.js
+++ routes/admin.js
@@ -6,10 +6,30 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', [auth, admin], async (req, res) => {
-  const user = await User.findById('5d7f8954b64ccc3948244920');
+  User.find({}, function(err, users) {
+    var userMap = users;
+    userMap.shift();
+
+    Plan.find({}, function(err, plans) {
+      var planMap = plans;
+      
+      res.render('admin', { users: userMap, plans: planMap});
+    });
+  });
+
+
+
+
+});
+
+router.get('/:id', [auth, admin], async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(404).send('Invalid user.');
+
   const plan = await Plan.findById(user.plan);
-  
-  res.render('admin', { firstName: user.firstName, lastName: user.lastName, country: user.country, budget: plan.budget, date: plan.dateCreated });
+  if (!user) return res.status(404).send('Invalid plan.');
+
+  res.render('admin_plan', { usecases: plan.gaming, sliderOne: plan.sliderOne, sliderTwo: plan.sliderTwo, case1: plan.case, mainColor: plan.mainColor, secondColor: plan.secondColor, budget: plan.budget, name: plan.name, resolution: plan.resolution, fps: plan.fps, setting: plan.setting, comment: plan.comment });
 });
 
 module.exports = router;
